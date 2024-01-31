297ce606fa5f132458dc0834577342cd70e74cae
Gustavo Henrique Pinto <gustavo.henrique@nappsolutions.com>
2024-01-31 10:54:59 -0300

    refactor: move mux and server initialization to another file

--- cmds/rest/main.go
+++ cmds/rest/main.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"github.com/gustapinto/go_hex/cmds/rest/route/ping"
-	"github.com/gustapinto/go_hex/pkg/httputil"
 	"log/slog"
-	"net/http"
 	"os"
 )
 
-const (
-	ServerAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("GET /v1/ping", httputil.Log(ping.Pong))
-
-	slog.Info("Starting HTTP server", "address", ServerAddress)
-
-	err := http.ListenAndServe(ServerAddress, mux)
+	err := StartServer()
 	if err != nil {
 		slog.Error("Failed to start http server", "error", err.Error())
 		os.Exit(1)
---
+++ cmds/rest/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gustapinto/go_hex/cmds/rest/route/ping"
+	"github.com/gustapinto/go_hex/pkg/httputil"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+const (
+	ServerAddress = "0.0.0.0:8080"
+)
+
+func StartServer() error {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /v1/ping", httputil.Log(ping.Pong))
+
+	slog.Info("Starting HTTP server", "address", ServerAddress)
+
+	return http.ListenAndServe(ServerAddress, mux)
+}
